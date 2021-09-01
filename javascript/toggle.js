c4bfddac2f205c1e53ee4cb1966b733e3f8083e8
Yawson-Enoch <enochgybex25@gmail.com>
2021-09-01 15:51:11 +0000

    switch nav bar to left and general refactor

--- javascript/toggle.js
+++ javascript/toggle.js
@@ -1,8 +1,6 @@
 let toggleMenu = document.querySelector(".fas.fa-bars");
-// console.log(toggleMenu);
 
 let navBar = document.querySelector("nav");
-// console.log(navBar);
 
 toggleMenu.addEventListener("click", () => {
   navBar.classList.toggle("active");
