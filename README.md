7e8e4c2af5d9a4141bdf86928049337472e71900
Yawson-Enoch <enochgybex25@gmail.com>
2021-10-21 13:29:51 +0000

    refactor and minify files

---
+++ README.md
@@ -0,0 +1,9 @@
+# MINI BLOG SITE
+
+This project displays a list of cars and some dummy descriptions. I was just using this project to test my css layout strength.
+
+## working sitemaps
+
+> Home
+
+> About Me
--- javascript/toggle.js
+++
@@ -1,7 +0,0 @@
-let toggleMenu = document.querySelector(".fas.fa-bars");
-
-let navBar = document.querySelector("nav");
-
-toggleMenu.addEventListener("click", () => {
-  navBar.classList.toggle("active");
-});
